81554160f031c27768c3913ce1034d4cdec83f61
mrcastle <matthew.ray.castle@gmail.com>
2022-09-02 21:49:40 -0400

    remove global style for now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,8 +11,6 @@ import { withBlitz } from "app/blitz-client";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 
-import "../styles/global.scss";
-
 const darkTheme = createTheme({
   palette: {
     mode: "dark",
