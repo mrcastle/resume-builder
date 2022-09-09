cc5b0011e388cd95da14ab44b77d8812571fb9ee
mrcastle <matthew.ray.castle@gmail.com>
2022-09-09 11:26:23 -0400

    update resume validations for summary

--- app/resumes/validations.ts
+++ app/resumes/validations.ts
@@ -7,12 +7,14 @@ import { z } from "zod";
 export const CreateResume = z.object({
   title: z.string(),
   userDisplayName: z.string(),
+  summary: z.string(),
   technicalCategories: z.array(TechnicalCategory),
 });
 
 export const UpdateResume = z.object({
   id: z.number(),
   title: z.string(),
+  summary: z.string(),
   userDisplayName: z.string(),
   technicalCategories: z.array(UpdateTechnicalCategory),
 });
